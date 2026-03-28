a1391aaa6a9f883f77458cffc0d9a3b7be158e4b
GrandTojo <tojomayose@gmail.com>
2026-03-28 09:59:35 +0300

    Setup: Serveur Express de base et App React prête

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => res.send("API Opérationnelle !"));
+
+app.listen(PORT, () => console.log(`Serveur lancé sur http://localhost:${PORT}`));
